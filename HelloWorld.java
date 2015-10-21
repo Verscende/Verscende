9068c80d6a3bda84d71827dbd4a2137454a7f33e
Verscende <verscende@gmail.com>
2015-10-21 07:12:53 +0000

    made some chages

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,10 @@
 public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World");
-<<<<<<< HEAD
-        System.out.println("Welcome to Verscende");
+System.out.println("Welcome to Verscende");
          
-=======
-        System.out.println("www.verscende.com");
->>>>>>> TalkServer
+
+    
                
     }
 }
