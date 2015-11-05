691e0cd792b7ff24ef84812f51a1c68ffcf398be
Verscende <verscende@gmail.com>
2015-11-05 15:13:47 +0000

    this program can now receive user input, and provide a basic output

--- Next.py
+++ Next.py
@@ -1,2 +1,7 @@
-print "www.google.com"
-print "www.facebook.com"
+print "Output is:"
+
+question = "Would you like to know who I am"
+print(question)
+answer = raw_input()
+
+print( "Since you have said "+answer+" , will answer by saying, I am Next a program created by John Nathan Raj")
